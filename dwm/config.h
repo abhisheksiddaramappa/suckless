63b8d89bad597ab70ec3c233feccca2a728350e4
abhisheksiddaramappa <abhishek.ss@live.com>
2020-07-14 16:07:42 +0530

    Added gitignore, modified window border.

--- dwm/config.h
+++ dwm/config.h
@@ -1,21 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#444444";     /* border color unfocused windows */
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_blue[]            = "#1793d0"; /* border color focused windows and tags */
+static const char col_red[]             = "#8f3d3d"; /* tag background color */
+static const char col_yellow[]          = "#b38c00"; /* border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_red,  col_yellow },
+	[SchemeTitle]  = { col_gray4, col_blue, col_blue },
 };
 
 /* tagging */
