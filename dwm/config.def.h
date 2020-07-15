8afdb3e1c2b33710d9adb96e5bbd1361fa71bde0
abhisheksiddaramappa <abhishek.ss@live.com>
2020-07-15 12:25:41 +0530

    Sync config.def.h and config.h

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,21 +1,25 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+sstatic const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#444444";     /* border color unfocused windows */
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_blue[]            = "#1793d0"; /* border color focused windows and tags */
+static const char col_red[]             = "#8f3d3d"; /* tag background color */
+static const char col_yellow[]          = "#b38c00"; /* border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_red,  col_yellow },
+	[SchemeTitle]  = { col_gray4, col_blue, col_blue },
 };
 
 /* tagging */
@@ -28,7 +32,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-//	{ "Firefox",  NULL,       NULL,       1<<0,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,       0,           -1 },
 };
 
@@ -61,8 +64,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
-static const char *scrotcmd[]  = { "scrot", NULL };
-static const char *scrotselectcmd[]  = { "scrot", "-s", NULL };
+static const char *scrotcmd[]  = { "scrot", "-s", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -102,7 +104,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ MODKEY,                       XK_Print,   spawn,      {.v = scrotcmd } },
-	{ MODKEY|ShiftMask,             XK_Print,   spawn,      SHCMD("sleep 1s;scrot --select") },
+	{ MODKEY|ShiftMask,             XK_Print,   spawn,      SHCMD("sleep 1s;scrot --select") }
 };
 
 /* button definitions */
